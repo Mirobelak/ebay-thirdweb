d2eb074548b7579332a688652708a500cf92a94f
Miroslav Belak <miroslavbelak123@gmail.com>
2022-11-03 19:46:26 +0100

    added auction and direct listing functions

--- components/Header.tsx
+++ components/Header.tsx
@@ -55,7 +55,7 @@ function Header({}: Props) {
                 <input type='text' placeholder='Search for anything' className='flex-1 outline-none ' />
             </div>
             <button className='hidden sm:inline bg-blue-600 text-white px-5 md:px-10 py-2 border-2 border-blue-600'>Search</button>
-            <Link href="/">
+            <Link href="/createPage">
             <button className='border-2 border-blue-600 px-5 md:px-10 py-2 text-blue-600 hover:bg-blue-600/50 hover:text-white cursor-pointer'>List Item</button>
             </Link>
         </section>
--- pages/createPage.tsx
+++ pages/createPage.tsx
@@ -1,12 +1,92 @@
 import React from 'react'
 import Header from '../components/Header'
+import {useAddress, useContract, MediaRenderer, useNetwork, useNetworkMismatch, useOwnedNFTs, useCreateAuctionListing, useCreateDirectListing} from '@thirdweb-dev/react'
+import {useRouter} from 'next/router'
+import network from '../utils/network'
+import {NFT, NATIVE_TOKENS, NATIVE_TOKEN_ADDRESS} from '@thirdweb-dev/sdk'
 
 type Props = {}
 
 const CreatePage = (props: Props) => {
-  return (
+    const {contract} = useContract(process.env.NEXT_PUBLIC_MARKETPLACE_CONTRACT, "marketplace")
+    const {contract: collectionContract} = useContract(process.env.NEXT_PUBLIC_COLLECTION_CONTRACT, "nft-collection")
+    const address = useAddress()
+    const router = useRouter()
+    const ownedNFTs = useOwnedNFTs(collectionContract, address)
+    const networkMismatch = useNetworkMismatch()
+    const [, switchNetwork] = useNetwork()
+    const {mutate: createDirectListing, isLoading: isLoadingDirect, error: errorDirect} = useCreateDirectListing(contract)
+    const {mutate: createAuctionListing, isLoading: isLoadingAuction, error: errorAuction} = useCreateAuctionListing(contract)
+    const [selectedNFT, setSelectedNFT] = React.useState<NFT>()
+
+    const handleDreateListing = async (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if(networkMismatch) {
+            switchNetwork && switchNetwork(network)
+            return
+        }
+        if(!selectedNFT) return;
+            const target = e.target as typeof e.target & {
+                elements: {listingType: {value: string};
+                price: {value: string}}
+            }
+            const {listingType, price} = target.elements
+
+            if(listingType.value === "direct") {
+                createDirectListing({assetContractAddress: process.env.NEXT_PUBLIC_COLLECTION_CONTRACT!, tokenId: selectedNFT.metadata.id, currencyContractAddress: NATIVE_TOKEN_ADDRESS, listingDurationInSeconds: 60 * 60 * 24 * 7, quantity: 1, buyoutPricePerToken:price.value,startTimestamp: new Date()}, {onSuccess(data, variables, context) { console.log("SUCCES" + data, variables, context); 
+                router.push("/")},
+                onError(error, variables, context) { console.log("ERROR" + error, variables, context);}})
+            }
+            else if(listingType.value === "auction") {
+                createAuctionListing(
+                    {assetContractAddress: process.env.NEXT_PUBLIC_COLLECTION_CONTRACT!, 
+                    tokenId: selectedNFT.metadata.id, 
+                    currencyContractAddress: NATIVE_TOKEN_ADDRESS, 
+                    listingDurationInSeconds: 60 * 60 * 24 * 7, 
+                    quantity: 1,
+                    startTimestamp: new Date(),
+                    buyoutPricePerToken:price.value,
+                    reservePricePerToken: 0,}, 
+                    {onSuccess(data, variables, context) { console.log("SUCCES" + data, variables, context); 
+                router.push("/")},
+                onError(error, variables, context) { console.log("ERROR" + error, variables, context);}})
+            }
+
+        } 
+    return (
     <div>
         <Header/>
+        <main className='max-w-6xl mx-auto p-2'>
+        <h1 className='text-4xl font-bold'>List an Item</h1>
+        <h2 className='text-xl font-semibold'>Select an Item you wold like to sell</h2>
+
+        <hr className='mb-5' />
+        <p>Below you will see your NFTs</p>
+        <div className='flex overflow-x-scroll p-4 space-x-2'>
+            {ownedNFTs?.data?.map((nft) => (
+            <div key={nft.metadata.id} onClick={() => setSelectedNFT(nft)} className={`flex flex-col space-y-2 card min-w-fit border-2 bg-gray-100 ${nft.metadata.id === selectedNFT?.metadata.id ? "border-black" : "border-transparent"} `}>
+                <MediaRenderer className='h-48 rounded-lg' src={nft.metadata.image} />
+                <p className='text-lg font-bold truncate'>{nft.metadata.name}</p>
+                <p className='text-xs truncate'>{nft.metadata.description}</p>
+                </div>
+            ))}
+        </div>
+        {selectedNFT && (
+            <form onSubmit={handleDreateListing}  className='flex w-full h-fit mx-auto justify-center'>
+            <div className='flex flex-col space-y-4 w-[500px]' >
+                <div className='flex flex-col p-10'>
+                    <label className='border-r font-light'>Direct Listing / Fixed Price</label>
+                    <input className='ml-auto h-10 w-10 relative bottom-8' type="radio" name="listingType" value="direct" />
+                    <label className='border-r font-light'>Auction</label>
+                    <input className='ml-auto h-10 w-10 relative bottom-8 ' type="radio" name="listingType" value="auction" />
+                    <label className='font-light border-r'>Price</label>
+                    <input className='bg-gray-100 p-5' type="text" name="price" placeholder='0.05' />
+                </div>
+                <button type="submit" className='bg-blue-600 text-white rounded-lf p-4 mt-8'>Create Listing</button>
+            </div>
+            </form>
+        )}
+        </main>
     </div>
   )
 }
